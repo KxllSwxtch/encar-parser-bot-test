8ca71ffeacd3ad4906ac6e32da79826ac61ba75c
KxllSwxtch <dimashin9@gmail.com>
2024-11-27 10:54:44 +0900

    updates

--- main.py
+++ main.py
@@ -189,7 +189,7 @@ def get_car_info(url):
     except WebDriverException as e:
         print(f"Ошибка Selenium: {e}")
         driver.quit()
-        return ""
+        return "Произошла ошибка получения данных..."
 
 
 # Обработчик команды /start
